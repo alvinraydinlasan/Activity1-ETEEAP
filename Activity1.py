c6125d2fcef3bfffd33a6b1a99644fff7291a3d2
alvin <alvin@gmail.com>
2021-11-10 02:16:21 +0000

    Commiting Activity1

---
+++ Activity1.py
@@ -0,0 +1,9 @@
+input("Student name: ")
+prelim = int(input("Prelims: "))
+midterm = int(input("Midterm: "))
+semi = int(input("Semifinal: "))
+final = int(input("Final: "))
+
+total = (prelim+midterm+semi+final)/4
+
+print("Average: ", total)
